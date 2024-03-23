eafe05815604b69ba4fe7e9f742c4a0ea37f76bb
Pratik Singh <pratikrajaryan@gmail.com>
2024-03-23 11:56:09 +0530

    remove comments

--- src/scenes/loginPage/Form.jsx
+++ src/scenes/loginPage/Form.jsx
@@ -8,13 +8,13 @@ import {
   useTheme,
 } from "@mui/material";
 import { url } from "helper/url";
-import EditOutlinedIcon from "@mui/icons-material/EditOutlined";
+// import EditOutlinedIcon from "@mui/icons-material/EditOutlined";
 import { Formik } from "formik";
 import * as yup from "yup";
 import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { setLogin } from "state";
-import Dropzone from "react-dropzone";
+// import Dropzone from "react-dropzone";
 import FlexBetween from "components/FlexBetween";
 import { toast } from 'react-toastify';
 

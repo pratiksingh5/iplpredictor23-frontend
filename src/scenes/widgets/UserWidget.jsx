478c2daf842dbeba61697b8c167d5c931240ed78
Pratik Singh <pratikrajaryan@gmail.com>
2024-03-23 21:17:43 +0530

    updated image link

--- src/scenes/widgets/UserWidget.jsx
+++ src/scenes/widgets/UserWidget.jsx
@@ -68,10 +68,10 @@ const UserWidget = ({ userId, picturePath }) => {
     GT: "https://upload.wikimedia.org/wikipedia/en/thumb/0/09/Gujarat_Titans_Logo.svg/1200px-Gujarat_Titans_Logo.svg.png",
     DC: "https://www.nsbpictures.com/wp-content/uploads/2019/03/1024px-Delhi_Capitals_Logo.svg_.png",
     KKR: "https://logowik.com/content/uploads/images/kolkata-knight-riders6292.jpg",
-    LSG: "https://hindubabynames.info/downloads/wp-content/themes/hbn_download/download/sports-ipl/lucknow-super-giants-logo.png",
+    LSG: "https://logowik.com/content/uploads/images/lucknow-super-giants3550.logowik.com.webp",
     MI: "https://www.pngall.com/wp-content/uploads/2017/04/Mumbai-Indians-Logo-PNG.png",
     PBKS : "https://pixlok.com/wp-content/uploads/2021/04/Punjab-Kings-Logo-PNG-Image.jpg",
-    RR: "https://upload.wikimedia.org/wikipedia/en/thumb/6/60/Rajasthan_Royals_Logo.svg/1200px-Rajasthan_Royals_Logo.svg.png",
+    RR: "https://i.pinimg.com/originals/b2/21/ee/b221ee9ae9ecb3fa0f158161ef457e29.png",
     RCB: "https://e1.pxfuel.com/desktop-wallpaper/436/167/desktop-wallpaper-rcb-logo-ipl-logo-thumbnail.jpg",
     SRH: "https://www.pngitem.com/pimgs/m/77-776155_sunrisers-hyderabad-logo-png-image-free-download-searchpng.png"
   };

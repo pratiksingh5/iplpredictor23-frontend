3db7d1bab404c8fa1a12dc0bb4bc9b66cdfdcba9
Pratik Singh <pratikrajaryan@gmail.com>
2025-03-21 17:48:13 +0530

    updated build command

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,13 @@ import path from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import Checker from 'vite-plugin-checker';
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
-    react()
+    react(),
+    Checker({ typescript: true }) 
   ],
   resolve: {
     alias: {
